6b01ea01e674c329b5434a8de5e86de5995b7d78
RashmiMuraharishetti <raymondhedgecock@gmail.com>
2018-12-14 10:47:53 -0700

    initial commit

---
+++ src/main/java/com/newzorktimes/newzorktimes/NewzorktimesApplication.java
@@ -0,0 +1,14 @@
+package com.newzorktimes.newzorktimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewzorktimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewzorktimesApplication.class, args);
+	}
+
+}
+
